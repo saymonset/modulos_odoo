cb5eb33ae891b9c03f9a94290d7fd499058f8264
saymonset <saymon_set@hotmail.com>
2026-05-10 15:07:02 +0200

    script readme , organizacion de carpetas extra  y oca centralizadas y los proyectos seran leidos por el path directamente

--- 3_ver_modulos.sh
+++ 3_ver_modulos.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Script para listar todos los módulos migrados
 
-echo "=== MODULOS EN SHARED ==="
-ls -la /home/odoo/modulos_odoo/shared/
+echo "=== ESTRUCTURA SHARED ==="
+ls -F /home/odoo/modulos_odoo/shared/
 
 echo ""
-echo "=== MODULOS POR VERSION ==="
-for mod in /home/odoo/modulos_odoo/shared/*/; do
-    if [ -d "$mod" ]; then
-        nombre=$(basename "$mod")
-        echo "Modulo: $nombre"
-        ls "$mod"
-        echo ""
-    fi
+echo "=== MODULOS POR CATEGORIA Y VERSION ==="
+for cat in oca extra; do
+    echo ">>>> CATEGORIA: $cat <<<<"
+    for ver in 18.0 19.0; do
+        PATH_VER="/home/odoo/modulos_odoo/shared/$cat/$ver"
+        if [ -d "$PATH_VER" ]; then
+            COUNT=$(ls -1 "$PATH_VER" | wc -l)
+            echo "  [$ver] ($COUNT módulos)"
+            ls -F "$PATH_VER"
+            echo ""
+        fi
+    done
 done
--- 9_3_mover_destino_aqui.sh
+++ 9_3_mover_destino_aqui.sh
@@ -2,8 +2,8 @@
 mkdir -p /home/odoo/modulos_odoo/shared/oca/19.0
 
 # 2. Mover solo directorios que contengan manifiesto de módulo Odoo
-origen="/home/odoo/develop/odoo-from-13-to-18/arquitectura/odoo19/clientes/integraiadev_19/data/addons/oca"
-destino="/home/odoo/modulos_odoo/shared/oca/19.0"
+origen="/home/odoo/develop/odoo-from-13-to-18/arquitectura/odoo19/clientes/integraiadev_19/data/addons/extra"
+destino="/home/odoo/modulos_odoo/shared/extra/19.0"
 
 for dir in "$origen"/*/ ; do
     # Eliminar la barra final para tener el nombre del directorio
