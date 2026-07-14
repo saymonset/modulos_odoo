efbc171664246e7d521061d50cc13501ad44eb23
saymonset <saymon_set@hotmail.com>
2026-07-14 17:57:34 +0200

    factura  calcula en usd lo que tiene en bs. antes no lo hacia

--- shared/extra/19.0/bcv_rate_update_venezuela/models/account_move.py
+++ shared/extra/19.0/bcv_rate_update_venezuela/models/account_move.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.tools.float_utils import float_round
 
 
 class AccountMove(models.Model):
@@ -26,6 +27,19 @@ class AccountMove(models.Model):
     @api.depends('line_ids.price_subtotal_usd_bcv')
     def _compute_amount_total_usd(self):
         for move in self:
-            move.amount_total_usd = sum(
-                line.price_subtotal_usd_bcv for line in move.line_ids
+            move.amount_total_usd = float_round(
+                sum(move.line_ids.mapped('price_subtotal_usd_bcv')),
+                precision_digits=2,
             )
+
+    @api.onchange('invoice_line_ids')
+    def _onchange_invoice_line_ids_usd(self):
+        """Forzar rec�lculo del total USD desde form cuando cambian las l�neas."""
+        for move in self:
+            rate = self.env['product.template']._get_bcv_rate(move.company_id)
+            total = sum(
+                (l.price_unit / rate) * l.quantity
+                for l in move.invoice_line_ids
+                if rate and l.price_unit
+            )
+            move.amount_total_usd = float_round(total, precision_digits=2)
